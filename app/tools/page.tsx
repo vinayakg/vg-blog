7f361eddf35ee35f2d8c69ad4459e40962d2d233
vinayakg <vinayakg@live.com>
2025-06-21 18:22:54 +0530

    changing description

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -10,10 +10,11 @@ export default function Tools() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 dark:text-gray-100">
-            Projects
+            Tools
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Showcasing some of the most useful tools and tutorials that can help you in your
+            development journey.
           </p>
         </div>
         <div className="container py-12">
